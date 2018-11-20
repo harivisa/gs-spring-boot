ef453fab37cfbc54e94f6782141f6c7e5d072e95
harivisa <haridaisy3@gmail.com>
2018-11-20 11:07:33 +0200

    changes

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Tervetuloa devops sivuun.";
+        return "Tervetuloa devops";
     }
     
 }
