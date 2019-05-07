a9ff5338a61a4a4d9dd8f81bf22d9de08fe9c8ce
harivisa <43164604+harivisa@users.noreply.github.com>
2019-05-07 12:32:06 +0300

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Tervetuloa devops";
+        return "Tervetuloa Digitalisaation teemaryhmään";
     }
     
 }
